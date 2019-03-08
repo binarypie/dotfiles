2059548a743d5cb828f4f5c5845ddfb2e5258f1e
Charles Christolini <code@binarypie.com>
2019-03-07 20:25:41 -0800

    minor update to prezto install

--- prezto/install.sh
+++ prezto/install.sh
@@ -1,3 +1,6 @@
-#!/bin/sh
-
-git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
+#!/bin/zsh
+if [ -d "${ZDOTDIR:-$HOME}/.zprezto" ]; then
+	echo "Prezto already installed."
+else
+	git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
+fi
