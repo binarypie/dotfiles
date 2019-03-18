c5a3922d3b13b8948d4036178ad118289a29688d
Charles Christolini <code@binarypie.com>
2019-03-17 18:50:35 -0700

    vscode updates

---
+++ vscode/install.sh
@@ -0,0 +1,4 @@
+#!/bin/zsh
+ln -s $HOME/.dotfiles/vscode/settings.json $HOME/.config/Code/User/settings.json
+ln -s $HOME/.dotfiles/vscode/keybindings.json $HOME/.config/Code/User/keybindings.json
+ln -s $HOME/.dotfiles/vscode/snippets $HOME/.config/Code/User/snippets
