e0550da0d783db7c108e27328d6c91fca9a5646b
Charles Christolini <code@binarypie.com>
2019-12-19 21:51:08 -0800

    Updated the git config to use vimdiff and templates.

--- git/install.sh
+++ git/install.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+# Email / Nameo
+read -r -p "Enter Your Git Name: "  name
+read -r -p "Enter Your Git Email: "  email
+git config --global user.name "$name"
+git config --global user.email "$email"
+
 # Don't ask ssh password all the time
 if [ "$(uname -s)" = "Darwin" ]; then
 	git config --global credential.helper osxkeychain
@@ -12,8 +18,10 @@ if which diff-so-fancy >/dev/null 2>&1; then
 	git config --global core.pager "diff-so-fancy | less --tabs=4 -RFX"
 fi
 
-# use vscode as mergetool
-if which code >/dev/null 2>&1; then
-	git config --global merge.tool vscode
-	git config --global mergetool.vscode.cmd "code --wait $MERGED"
-fi
+# use vim as mergetool
+git config --global merge.tool vimdiff
+git config --global merge.conflictstyle diff3
+git config --global mergetool.prompt false
+
+# Templates
+git config --global commit.template ~/.dotfiles/git/template.txt
