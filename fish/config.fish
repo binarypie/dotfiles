a7929bd9716bbfab35f9ab922ae165f4de21b63c
Charles Christolini <binarypie@localhost.localdomain>
2023-05-17 11:47:14 -0700

    new stuff

--- fish/config.fish
+++ fish/config.fish
@@ -1,39 +1,16 @@
-# Starship
-starship init fish | source
+if status is-interactive
+    # Variables
+    set -x HELIX_RUNTIME /lib64/helix/runtime
 
-# Defaults
-set -x PROJECTS $HOME/Code
+    # Prompt
+    set --global hydro_multiline true
+    set --global fish_prompt_pwd_dir_length 100
+    set --global hydro_color_pwd $fish_color_comment
+    set --global hydro_color_git $fish_color_operator
+    set --global hydro_color_error $fish_color_error
+    set --global hydro_color_prompt $fish_color_command
+    set --global hydro_color_duration $fish_color_param
 
-# Local Bin
-fish_add_path -m $HOME/.local/bin
-
-# Dart
-set -x DART_BIN /usr/lib/dart/bin
-set -x DART_PUB_CACHE_BIN $HOME/.pub-cache/bin
-set -x FLUTTER_BIN $PROJECTS/flutter/bin
-set -x FLUTTER_PUB_CACHE_BIN $HOME/Code/flutter/.pub-cache/bin
-set -x CHROME_EXECUTABLE /usr/bin/brave-browser
-fish_add_path $DART_BIN $DART_PUB_CACHE_BIN $FLUTTER_BIN $FLUTTER_PUB_CACHE_BIN
-
-# Golang
-set -x GOPATH $HOME/.go
-set -x GOSRC $GOPATH/src
-set -x GOBIN $GOPATH/bin
-fish_add_path /usr/local/go/bin $GOBIN
-
-# Rust
-fish_add_path $HOME/.cargo/bin
-
-# NPM
-fish_add_path -m $HOME/.npm-packages/bin
-
-# Android
-set -x ANDROID_HOME $PROJECTS/android
-fish_add_path $ANDROID_HOME/tools $ANDROID_HOME/platform-tools/
-
-# FZF
-set -x FZF_DEFAULT_COMMAND 'rg --files --follow --no-ignore-vcs --hidden -g "!{node_modules/*,.git/*}"'
-
-# VIM launches NVIM
-alias vim="nvim"
-alias vimdiff="vim -d"
+    # Path
+    fish_add_path $HOME/.npm/modules/bin
+end
